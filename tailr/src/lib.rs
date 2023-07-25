72ddf99809432a916b080ef7937b9b6c080f39d5
Miroslav Bogatyrev <mistermiros@gmail.com>
2023-07-25 12:26:57 +0200

    tailr optimisations

--- tailr/src/lib.rs
+++ tailr/src/lib.rs
@@ -1,6 +1,6 @@
 use std::{
-    collections::VecDeque,
-    io::{BufRead, Read},
+    fs::File,
+    io::{BufRead, BufReader, Read, Seek, SeekFrom},
 };
 
 use clap::Parser;
@@ -8,8 +8,8 @@ use shared_utils::MyResult;
 
 enum Mode {
     Bytes(usize),
-    Lines(usize),
     BytesFrom(usize),
+    Lines(usize),
     LinesFrom(usize),
 }
 
@@ -44,7 +44,9 @@ fn get_args() -> MyResult<Args> {
 
     //println!("{args:?}");
     let mode = if let Some(value) = args.bytes {
-        let count: usize = value.replace("-", "").parse()
+        let count = value
+            .replace("-", "")
+            .parse()
             .map_err(|_| format!("illegal byte count -- {value}"))?;
         if value.starts_with("+") {
             Mode::BytesFrom(count)
@@ -52,7 +54,10 @@ fn get_args() -> MyResult<Args> {
             Mode::Bytes(count)
         }
     } else {
-        let count: usize = args.lines.replace("-", "").parse()
+        let count = args
+            .lines
+            .replace("-", "")
+            .parse()
             .map_err(|_| format!("illegal line count -- {}", args.lines))?;
         if args.lines.starts_with("+") {
             Mode::LinesFrom(count)
@@ -68,53 +73,64 @@ fn get_args() -> MyResult<Args> {
     })
 }
 
-fn process_file(filename: &str, file: Box<dyn BufRead>, mode: &Mode) -> MyResult<()> {
+fn count_lines_and_bytes(filename: &str) -> MyResult<(usize, usize)> {
+    let mut file = BufReader::new(File::open(filename)?);
+    let mut num_lines = 0usize;
+    let mut num_bytes = 0usize;
+    let mut buf = Vec::new();
+    loop {
+        let bytes_read = file.read_until(b'\n', &mut buf)?;
+        if bytes_read == 0 {
+            break;
+        }
+        num_bytes += bytes_read;
+        num_lines += 1;
+    }
+    Ok((num_lines, num_bytes))
+}
+
+fn process_file(filename: &str, mode: &Mode) -> MyResult<()> {
+    let (total_lines, total_bytes) = count_lines_and_bytes(filename)?;
+    let file = File::open(filename)?;
     match mode {
         Mode::BytesFrom(index) => {
-            let iter = file.bytes();
-            let bytes = (iter.skip((*index).saturating_sub(1usize)).collect::<Result<Vec<u8>, _>>())?;
-            let str = String::from_utf8_lossy(&bytes).to_string();
-            if !str.is_empty() {
-                print!("{str}");
-            }
-        }
-        Mode::LinesFrom(index) => {
-            let iter = file.lines();
-            let lines = (iter.skip((*index).saturating_sub(1usize)).collect::<Result<Vec<String>, _>>())?;
-            for line in lines {
-                println!("{line}");
-            }
+            process_bytes(file, index.saturating_sub(1))?;
         }
         Mode::Bytes(count) => {
-            let file_size = std::fs::metadata(filename).unwrap().len() as usize;
-            let skip = file_size.saturating_sub(*count);
-            let iter = file.bytes();
-            let bytes = (iter.skip(skip).collect::<Result<Vec<u8>, _>>())?;
-            let str = String::from_utf8_lossy(&bytes).to_string();
-            if !str.is_empty() {
-                print!("{str}");
-            }
+            process_bytes(file, total_bytes.saturating_sub(*count))?;
+        }
+        Mode::LinesFrom(index) => {
+            let file = BufReader::new(file);
+            process_lines(file, *index)?;
         }
         Mode::Lines(count) => {
-            if *count == 0 { 
+            if *count == 0 {
                 return Ok(());
             }
-            let mut iter = file.lines();
-            let lines = iter.try_fold::<_, _, MyResult<_>>(
-                VecDeque::with_capacity(*count),
-                |mut acc, line| {
-                    let line = line?;
-                    if acc.len() == *count {
-                        acc.pop_front();
-                    }
-                    acc.push_back(line);
-                    Ok(acc)
-                },
-            )?;
-            for line in lines {
-                println!("{line}");
-            }
+            let file = BufReader::new(file);
+            process_lines(file, total_lines.saturating_sub(*count) + 1)?;
         }
+    }
+    Ok(())
+}
+
+fn process_bytes(mut file: impl Read + Seek, from: usize) -> MyResult<()> {
+    file.seek(SeekFrom::Start(from as u64))?;
+    let mut buf = Vec::new();
+    file.read_to_end(&mut buf)?;
+    print!("{}", String::from_utf8_lossy(&buf));
+    Ok(())
+}
+
+fn process_lines(mut file: impl BufRead, from: usize) -> MyResult<()> {
+    let mut current = 0usize;
+    let mut buf = String::new();
+    while file.read_line(&mut buf)? > 0 {
+        current += 1;
+        if current >= from {
+            print!("{buf}");
+        }
+        buf.clear();
     }
     Ok(())
 }
@@ -127,13 +143,8 @@ pub fn run() -> MyResult<()> {
         if print_names {
             println!("==> {filename} <==");
         }
-        let file = shared_utils::open(&Some(&filename));
-        if let Err(error) = &file {
-            println!("{error}");
-        }
-        let file = file.unwrap();
-        if let Err(error) = process_file(&filename, file, &args.mode) {
-            println!("{filename}: {error}");
+        if let Err(error) = process_file(&filename, &args.mode) {
+            eprintln!("{filename}: {error}");
         }
         let is_last = num + 1 == total_files;
         if print_names && !is_last {
